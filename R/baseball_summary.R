61483068ad632b749f5e4edbae360e5a3338a157
Katherine Chan <kchan78@calpoly.edu>
2024-05-16 15:53:22 -0700

    Added initial code to summarize pitch types

--- R/baseball_summary.R
+++ R/baseball_summary.R
@@ -1,6 +1,8 @@
 #' Description of function
 #'
-#' @param
+#' @param data trackman baseball dataset
+#' @param pitcherid The ID of pitcher
+#' @param pitch_type variable name "tagged" or "auto"
 #'
 #' @return A table indicating the percentage breakdown of types of pitches for
 #' the pitcher
@@ -8,8 +10,20 @@
 #' @importFrom package function
 #'
 #' @export
-pitch_breakdown <- function() {
+pitch_breakdown <- function(data, pitcherid, type = "tagged") {
 
+  if (type == "tagged"){
+    column = "TaggedPitchType"
+  } else{
+    column = "AutoPitchType"
+  }
+
+  pitcher_data <- data %>%
+    filter(PitcherId == pitcherid)
+
+  pitcher_data %>%
+    group_by(column) %>%
+    summarize(percentage = n() / nrow(pitcher_data) * 100)
 
 
 }
