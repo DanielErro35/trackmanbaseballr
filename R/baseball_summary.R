424c079e0267f22eeba343de018112ed7723ddde
Katherine Chan <kchan78@calpoly.edu>
2024-05-16 15:52:17 -0700

    create skeleton for pitcher statistics function

---
+++ R/baseball_summary.R
@@ -0,0 +1,24 @@
+#' Description of function
+#'
+#' @param
+#'
+#' @return A table indicating the percentage breakdown of types of pitches for
+#' the pitcher
+#'
+#' @importFrom package function
+#'
+#' @export
+pitch_breakdown <- function() {
+
+
+
+}
+
+
+
+#' Description for helper function
+#'
+#' @param name description
+#' @param name description
+#'
+#' @return
