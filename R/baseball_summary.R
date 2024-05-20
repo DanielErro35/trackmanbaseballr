d537c7a5bf595a9e3c95b2295a6c8c3184c2f026
Katherine Chan <kchan78@calpoly.edu>
2024-05-20 12:42:52 -0700

    Write pitcher function to summarize relevant information by pitch type

--- R/baseball_summary.R
+++ R/baseball_summary.R
@@ -21,22 +21,41 @@ pitch_breakdown <- function(data, pitcherid, type = "tagged") {
 
   if (type == "tagged"){
     column = "TaggedPitchType"
-  } else{
+  } else if (type == "auto"){
     column = "AutoPitchType"
+  } else {
+    stop("Pitch type must be 'tagged' or 'auto'")
   }
 
   pitcher_data <- data %>%
-    filter(PitcherId == pitcherid)
+    filter(PitcherId == pitcherid) %>%
+    mutate(PitchCallClass = case_when(
+      PitchCall  %in% c("BallCalled", "HitByPitch") ~ "Ball",
+      TRUE ~ "Strike")) %>%
+    select(contains("Pitch"), RelSpeed, SpinRate, InducedVertBreak, Extension)
 
   pitcher_data %>%
-    group_by(column) %>%
-    summarize(percentage = n() / nrow(pitcher_data) * 100)
-
+    group_by(!!sym(column)) %>%
+    summarize(
+      pitch_type_percent = n() / nrow(pitcher_data)*100,
+      avg_velocity = mean(RelSpeed),
+      avg_spinrate = mean(SpinRate),
+      avg_induced_ver_break = mean(InducedVertBreak),
+      avg_extension = mean(Extension),
+      strike_ball_percent = sum(PitchCallClass == "Strike")*100 / n()
+    )
 
 }
 
+library(dplyr)
+poly_utah_game <- read.csv(here::here("5-10-24_CalPoly_UtahTech.csv"))
+
+pitch_breakdown(data = poly_utah_game, pitcherid = 1000114562)
+
+
 # besides just % of each type of pitch, summarize
-# proportion strikes, balls, fouls, hits (hits can be broken down further)
+# do not calculate: proportion strikes, balls, fouls, hits (hits can be broken down further)
+# strike-to-ball-percentage (balls, hit by pitch vs everything else)
 # average speed
 
 #' Description for helper function
