c2c001003f0c7d44c5e4b26f95670c0021878041
Katherine Chan <kchan78@calpoly.edu>
2024-05-26 14:21:09 -0700

    Added initial checks and data cleaning

--- R/pitcher_performance.R
+++ R/pitcher_performance.R
@@ -9,7 +9,20 @@
 #' @import dplyr
 #'
 #' @export
-pitcher_game <- function(data, pitcherid) {
+pitcher_performance <- function(data, pitcherid) {
+
+  # Check that pitcherid is a number in the dataset
+  if (!is.numeric(pitcherid)) {
+    stop("pitcherid must be numeric.")
+  }  else if (!(pitcherid %in% data$PitcherId)) {
+    stop("pitcherid is not present as a pitcher in this game.")
+  }
+
+  # Clean and filter trackman data
+  pitcher_data <- data %>%
+    filter(PitcherId == pitcherid) %>%
+    select(Inning, Outs, Balls, Strikes, RunsScored)
+
 
 }
 
