28c5a52039df6a3d8e1330b841eecaddca279020
Katherine Chan <kchan78@calpoly.edu>
2024-05-26 14:13:57 -0700

    Created template for pitcher_performance function

---
+++ R/pitcher_performance.R
@@ -0,0 +1,23 @@
+#' Function to summarize a pitcher's performance in a baseball game
+#'
+#' @param data A trackman baseball dataset
+#' @param pitcherid The numeric ID of the pitcher to summarize
+#'
+#' @return A dataframe summarizing pitcher performance for a specified pitcher
+#' in one baseball game, counting up innings, strikeouts, walks, and runs.
+#'
+#' @import dplyr
+#'
+#' @export
+pitcher_game <- function(data, pitcherid) {
+
+}
+
+#' Helper function description
+#'
+#' @param name description
+#'
+#' @return
+helper_function <- function(pitcher_data) {
+
+}
